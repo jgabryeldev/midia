f9fa05e790745bf472e763f1e248cfac1f3a2d03
João Gabryel Dos Santos Pereira <133254442+jgabryeldev@users.noreply.github.com>
2023-06-14 18:05:58 -0300

    som
    criada funções para escutar o som puxando o js pelo console.

---
+++ main.js
@@ -0,0 +1,53 @@
+function toquePom ()
+{
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('tecla_pom');
+
+function toqueClap()
+{
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap');
+
+function toqueTim()
+{
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim');
+
+function toquePuff()
+{
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('tecla_puff');
+
+function toqueSplash()
+{
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('tecla_splash');
+
+function toqueToim()
+{
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('tecla_toim');
+
+function toquePsh()
+{
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('tecla_psh');
+
+function toqueTic()
+{
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('tecla_tic');
+
+function toqueTom()
+{
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('tecla_tom');
