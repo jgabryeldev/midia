34d4a7031e6cc82faeb62b05d75d069387e126be
João Gabryel Dos Santos Pereira <133254442+jgabryeldev@users.noreply.github.com>
2023-06-14 18:21:34 -0300

    adicionado som com interação
    foram adicionados os sons no js e eles funcionam de acordo com a interação com o usuário.

--- main.js
+++ main.js
@@ -2,52 +2,52 @@ function toquePom ()
 {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('tecla_pom');
+document.querySelector('.tecla_pom').onclick = toquePom;
 
 function toqueClap()
 {
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap');
+document.querySelector('.tecla_clap').onclick = toqueClap;
 
 function toqueTim()
 {
     document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_tim');
+document.querySelector('.tecla_tim').onclick = toqueTim;
 
 function toquePuff()
 {
     document.querySelector('#som_tecla_puff').play();
 }
-document.querySelector('tecla_puff');
+document.querySelector('.tecla_puff').onclick = toquePuff;
 
 function toqueSplash()
 {
     document.querySelector('#som_tecla_splash').play();
 }
-document.querySelector('tecla_splash');
+document.querySelector('.tecla_splash').onclick = toqueSplash;
 
 function toqueToim()
 {
     document.querySelector('#som_tecla_toim').play();
 }
-document.querySelector('tecla_toim');
+document.querySelector('.tecla_toim').onclick = toqueToim;
 
 function toquePsh()
 {
     document.querySelector('#som_tecla_psh').play();
 }
-document.querySelector('tecla_psh');
+document.querySelector('.tecla_psh').onclick = toquePsh;
 
 function toqueTic()
 {
     document.querySelector('#som_tecla_tic').play();
 }
-document.querySelector('tecla_tic');
+document.querySelector('.tecla_tic').onclick = toqueTic;
 
 function toqueTom()
 {
     document.querySelector('#som_tecla_tom').play();
 }
-document.querySelector('tecla_tom');
+document.querySelector('.tecla_tom').onclick = toqueTom;
