5297c4cbf4b5ad1079011f89ed123931fd28fa7a
João Gabryel Dos Santos Pereira <133254442+jgabryeldev@users.noreply.github.com>
2023-06-15 18:05:21 -0300

    tentando deixar o código mais limpo

--- main.js
+++ main.js
@@ -2,52 +2,7 @@ function toquePom ()
 {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = toquePom;
 
-function toqueClap()
-{
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = toqueClap;
+const listaTeclasSom = document.querySelectorAll('.tecla');
 
-function toqueTim()
-{
-    document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = toqueTim;
-
-function toquePuff()
-{
-    document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = toquePuff;
-
-function toqueSplash()
-{
-    document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = toqueSplash;
-
-function toqueToim()
-{
-    document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = toqueToim;
-
-function toquePsh()
-{
-    document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = toquePsh;
-
-function toqueTic()
-{
-    document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = toqueTic;
-
-function toqueTom()
-{
-    document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = toqueTom;
+listaTeclasSom[0] = onclick = toquePom;
